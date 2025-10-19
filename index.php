5adda9ec7c0ab2267b9fbbde6fcfb886083c5893
irfanalep <irfanalifahmd@gmail.com>
2025-10-19 09:45:30 +0700

    Pertemuan 6 - Menambah GIF

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title>mywebsite</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="style/style.css" type="text/css" rel="stylesheet" />
+  </head>
+
+  <body>
+    <div id="container">
+      <div id="header">
+        <h1>PERTEMUAN I -- <br>SISTEM PAKAR</h1>
+        
+      </div>
+
+      <div id="sidebar">
+        <h3>navigasi</h3>
+        <ul id="navmenu">
+          <li><a href="index.php" class="selected">profil</a></li>
+          <li><a href="?module=galeri#pos">galeri</a></li>
+          <li><a href="?module=jadwal#pos">jadwal kuliah</a></li>
+        </ul>
+      </div>
+
+      <div id="page">
+        <?php if (isset($_GET['module']))
+        include "konten/$_GET[module].php";
+      else 
+        include "konten/home.php";?>
+      </div>
+      <div id="clear"></div>
+
+      <div id="footer">
+        <p>&copy; 2010</p>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ konten/jadwal.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+            <thead>
+                <tr>
+                    <th>No.</th>
+                    <th>Mata Kuliah</th>
+                    <th>Dosen</th>
+                    <th>Jadwal</th>
+                    <th>Ruangan</th>
+                    </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Pemodelan dan Simulasi</td>
+                    <td>Pak Azhari</td>
+                    <td>Senin, 07.30 - 10.00</td>
+                    <td>FST-4.003</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Etika Profesi Teknologi Informasi</td>
+                    <td>Bu Faiz</td>
+                    <td>Senin, 13.00 - 14.40</td>
+                    <td>FST-4.004</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Metodologi Penelitian</td>
+                    <td>Pak Husni</td>
+                    <td>Selasa, 07.30 - 10.00</td>
+                    <td>FST-5.010</td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>Pemrograman Berbasis Platform</td>
+                    <td>Pak Hendra</td>
+                    <td>Selasa, 10.15 - 12.45</td>
+                    <td>R-Lab OS</td>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <td>Keamanan Siber</td>
+                    <td>Pak Nashrul</td>
+                    <td>Selasa, 13.00 - 15.30</td>
+                    <td>R-Lab OS</td>
+                </tr>
+                <tr>
+                    <td>6</td>
+                    <td>Data Science</td>
+                    <td>Bu Ummi</td>
+                    <td>Rabu, 07.30 - 10.00</td>
+                    <td>FST-4.004</td>
+                </tr>
+                <tr>
+                    <td>7</td>
+                    <td>Kecerdasan Buatan</td>
+                    <td>Pak Viktor</td>
+                    <td>Rabu, 13.00-15.30</td>
+                    <td>R-Lab Multimedia 2</td>
+                </tr>
+                <tr>
+                    <td>8</td>
+                    <td>Sistem Operasi Lanjut</td>
+                    <td>Pak Viktor</td>
+                    <td>Kamis, 13.00-15.30</td>
+                    <td>Lab Apl2</td>
+                </tr>
+            </tbody>
+        </table>
+</body>
+</html>
